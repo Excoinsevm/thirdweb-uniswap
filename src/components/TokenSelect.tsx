50dea57e30b11d4d718dc2fb9aee07512a305984
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-07-02 02:49:03 +0100

    Update TokenSelect.tsx

--- src/components/TokenSelect.tsx
+++ src/components/TokenSelect.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react"
 import { Check, ChevronsUpDown } from "lucide-react"
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "./lib/utils"
+import { Button } from "./components/ui/button"
 import Image from "next/image"
 import {
     Command,
